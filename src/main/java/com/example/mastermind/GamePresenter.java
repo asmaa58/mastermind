dcf3df5e28188161e635f190a9cb653e9db88bf1
2301977 <2301977@student.eelu.edu.eg>
2024-12-19 03:39:04 +0200

    Get user guess until state change

--- src/main/java/com/example/mastermind/GamePresenter.java
+++ src/main/java/com/example/mastermind/GamePresenter.java
@@ -11,11 +11,24 @@ class GamePresenter {
     }
 
     private void startGame() {
+        String rangeLabel = model.getRangeLabel();
 
-        boolean shouldStartGame = view.promptUserForGameStart(
-                model.getRangeLabel());
+        boolean shouldStartGame = view.promptUserForGameStart(rangeLabel);
 
         if (!shouldStartGame)
             System.exit(0);
+
+        view.printHeader(model.getGuessList());
+        while (model.getGameState() == GameState.RUNNING) {
+            view.printReminder(rangeLabel);
+
+            String userGuess;
+            do {
+                userGuess = view.promptUserForGuess();
+            } while (!model.isUserGuessValid(userGuess));
+
+            model.addGuess(userGuess);
+            view.printHeader(model.getGuessList());
+        }
     }
 }
